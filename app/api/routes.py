5a529c2a568c7ad8ff5b6ea1d260b22ecbe3e2ca
Liz Hall <lizk.hall@gmail.com>
2023-02-06 17:04:26 -0600

    first commit

---
+++ app/api/routes.py
@@ -0,0 +1,74 @@
+from flask import Blueprint, request, jsonify, render_template
+from helpers import token_required
+from models import db, User, Car, car_schema, cars_schema
+
+api = Blueprint('api', __name__, url_prefix='/api')
+
+@api.route('/getdata')
+def getdata():
+    return {'yee': 'haw'}
+
+# Insert car into database
+@api.route('/cars', methods = ['POST'])
+@token_required
+def create_car_data(current_user_token):
+    make = request.json['make']
+    model = request.json['model']
+    nickname = request.json['nickname']
+    color = request.json['color']
+    user_token = current_user_token.token
+
+    print(f'BIG TESTER: {current_user_token.token}')
+
+    car = Car(make, model, nickname, color, user_token = user_token )
+
+    db.session.add(car)
+    db.session.commit()
+
+    response = car_schema.dump(car)
+    return jsonify(response)
+
+# Retrieve all cars
+@api.route('/cars', methods = ['GET'])
+@token_required
+def get_cars(current_user_token):
+    a_user = current_user_token.token
+    cars = Car.query.filter_by(user_token = a_user).all()
+    response = cars_schema.dump(cars)
+    return jsonify(response)
+
+# Retrieve a single car
+@api.route('/cars/<id>', methods = ['GET'])
+@token_required
+def get_single_car(current_user_token, id):
+    car = Car.query.get(id)
+    response = car_schema.dump(car)
+    return jsonify(response)
+
+# Update contact info
+# 'PUT' is the replace command
+@api.route('/cars/<id>', methods = ['POST','PUT'])
+@token_required
+def update_car(current_user_token,id):
+    car = Car.query.get(id) 
+    car.make = request.json['make']
+    car.model = request.json['model']
+    car.nickname = request.json['nickname']
+    car.color = request.json['color']
+    car.user_token = current_user_token.token
+
+    db.session.commit()
+    response = car_schema.dump(car)
+    return jsonify(response)
+
+# Yeet a contact right on out of there
+# IMPORTANT: DELETE BY ID because otherwise you delete the whole database
+# and get fired from your tech job
+@api.route('/cars/<id>', methods = ['DELETE'])
+@token_required
+def delete_car(current_user_token, id):
+    car = Car.query.get(id)
+    db.session.delete(car)
+    db.session.commit()
+    response = car_schema.dump(car)
+    return jsonify(response)
